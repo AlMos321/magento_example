5687bc6ac916d4fbc4bd4d8161897a72302a59c0
Alexandr Moskalenko <alexmoss09@gmail.com>
2017-11-07 11:46:26 +0200

    magento events

--- app/code/local/Almos/Calculation/Model/Observer.php
+++ app/code/local/Almos/Calculation/Model/Observer.php
@@ -8,4 +8,38 @@ class Almos_Calculation_Model_Observer
 		$cid = $observer->getCid();
 		echo $cid;
 	}
+
+	public function loginEvent(Varien_Event_Observer $observer)
+	{
+		$botToken = "377789587:AAHpNXDJ-oj0Iet7hfqnleoIrEPwZOh_QNE";
+		$chatId = 226843018;
+		$website = "https://api.telegram.org/bot" . $botToken;
+		$params = [
+			'chat_id' => $chatId,
+			'text' => "Вход в админку. Ip: " . $this->getRealIpAddr(),
+		];
+		$ch = curl_init($website . '/sendMessage');
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, ($params));
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		$result = curl_exec($ch);
+		curl_close($ch);
+	}
+
+	public function getRealIpAddr()
+	{
+		if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+		{
+			$ip = $_SERVER['HTTP_CLIENT_IP'];
+		} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
+		{
+			$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+		} else {
+			$ip = $_SERVER['REMOTE_ADDR'];
+		}
+		return $ip;
+	}
+
 }
